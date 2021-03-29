8ffa25c46dec4c9c51ea0cc43bbef450cd11ae98
Roberto Sanchez <rsanchez92@live.com>
2021-03-28 18:06:33 -0700

    Created a new component using terminal.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {HeroesComponent} from './heroes/heroes.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
