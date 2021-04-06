bcae76af908c034997d6c6713dc324abe8448cc3
Roberto Sanchez <rsanchez92@live.com>
2021-04-05 18:19:06 -0700

    Commented on Services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+// Bring in the service
 import {HeroService} from '../hero.service';
 @Component({
   selector: 'app-heroes',
